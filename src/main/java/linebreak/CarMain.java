db22f64977f36297379dce765da0ebf3e91ef1da
Marton Andras Habele <marton.habele@outlook.com>
2021-10-23 18:42:05 +0200

    Sortörések kezelése task done & markup updated

---
+++ src/main/java/linebreak/CarMain.java
@@ -0,0 +1,8 @@
+package linebreak;
+
+public class CarMain {
+    public static void main(String[] args) {
+        Car car = new Car();
+        System.out.println(car.getBrandAndTypeInSeparateLines("Volkswagen", "Arteon"));
+    }
+}
---
+++ src/main/java/linebreak/CityNames.java
@@ -0,0 +1,8 @@
+package linebreak;
+
+public class CityNames {
+    public static void main(String[] args) {
+        System.out.print("Budapest" + System.lineSeparator() + "Debrecen" +
+                System.lineSeparator() + "Győr");
+    }
+}
