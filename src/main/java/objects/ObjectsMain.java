dcde44521f48eaf472efd4c358eeae92d84224ab
martonhabele <marton.habele@gmail.com>
2021-10-31 15:30:38 +0100

    Commented out the whole ObjectsMain class as it's for test purposes only

--- src/main/java/objects/ObjectsMain.java
+++ src/main/java/objects/ObjectsMain.java
@@ -8,46 +8,46 @@ import java.util.List;
 
 public class ObjectsMain {
     public static void main(String[] args) {
-        new Book();
-        Book book = new Book();
-        System.out.println(book);
-
-        Book emptyBook = null;
-        System.out.println(emptyBook);
-
-        System.out.println(emptyBook == null);
-
-        book = new Book();
-        System.out.println(book);
-        book = null;
-        System.out.println(book);
-
-        Book anotherBook = new Book();
-        System.out.println(book == anotherBook);
-        book = anotherBook;
-        System.out.println(book);
-
-        Book book1 = new Book();    //1 pédány
-        Book book2 = new Book();    //1 pédány
-        Book book3 = new Book();    //1 pédány
-        Book book4 = book1;         //referenciaátadás
-        Book book5 = book1;         //referenciaátadás
-        Book book6 = book3;         //referenciaátadás
-        Book book7 = null;          //nullpéldány
-        book4 = book6;              //referenciaátadás
-        new Book();                 //új példány
-        book5 = new Book();         //új példány
-        book6 = null;               //nullreferencia átadása
-        book5 = book4;              //referenciaátadás
-
-        String[] books = {new Book(), new Book(), new Book()};
-        System.out.println(Arrays.toString(books));
-        List<Book> booksList = Arrays.asList(new Book(), new Book(), new Book());
-        System.out.println(booksList);
-        List<Book> emptyBookList = new ArrayList<>();
-        emptyBookList.add(new Book());
-        emptyBookList.add(new Book());
-        emptyBookList.add(new Book());
-        System.out.println(emptyBookList);
+//        new Book();
+//        Book book = new Book();
+//        System.out.println(book);
+//
+//        Book emptyBook = null;
+//        System.out.println(emptyBook);
+//
+//        System.out.println(emptyBook == null);
+//
+//        book = new Book();
+//        System.out.println(book);
+//        book = null;
+//        System.out.println(book);
+//
+//        Book anotherBook = new Book();
+//        System.out.println(book == anotherBook);
+//        book = anotherBook;
+//        System.out.println(book);
+//
+//        Book book1 = new Book();    //1 pédány
+//        Book book2 = new Book();    //1 pédány
+//        Book book3 = new Book();    //1 pédány
+//        Book book4 = book1;         //referenciaátadás
+//        Book book5 = book1;         //referenciaátadás
+//        Book book6 = book3;         //referenciaátadás
+//        Book book7 = null;          //nullpéldány
+//        book4 = book6;              //referenciaátadás
+//        new Book();                 //új példány
+//        book5 = new Book();         //új példány
+//        book6 = null;               //nullreferencia átadása
+//        book5 = book4;              //referenciaátadás
+//
+//        String[] books = {new Book(), new Book(), new Book()};
+//        System.out.println(Arrays.toString(books));
+//        List<Book> booksList = Arrays.asList(new Book(), new Book(), new Book());
+//        System.out.println(booksList);
+//        List<Book> emptyBookList = new ArrayList<>();
+//        emptyBookList.add(new Book());
+//        emptyBookList.add(new Book());
+//        emptyBookList.add(new Book());
+//        System.out.println(emptyBookList);
     }
 }
