1e7663d5422042b8d811248292304de48242ea88
Márton András Hábele <marton.habele@gmail.com>
2021-11-08 22:55:35 +0100

    Vezérlési szerkezetek és elágazás task updated

--- src/main/java/controlselection/greetings/Greetings.java
+++ src/main/java/controlselection/greetings/Greetings.java
@@ -3,6 +3,11 @@ package controlselection.greetings;
 import java.time.LocalTime;
 
 public class Greetings {
+    public static void main(String[] args) {
+        Greetings greetings = new Greetings();
+        greetings.greeter(17,10);
+    }
+
     public void greeter(int hour, int minute) {
         LocalTime time = LocalTime.of(hour, minute);
         LocalTime afterFiveMorning = LocalTime.of(4, 59);
@@ -10,7 +15,7 @@ public class Greetings {
         LocalTime afterHalfPastSixEvening = LocalTime.of(18, 29);
         LocalTime afterEight = LocalTime.of(19, 59);
 
-        if (time.isAfter(afterFiveMorning)) {
+        if (time.isAfter(afterFiveMorning) && time.isBefore(afterNineMorning)) {
             System.out.println("Jó reggelt!");
         }
 
