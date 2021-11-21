d710fcfc6204be5bf811e90143791a5666f065f6
Márton András Hábele <marton.habele@gmail.com>
2021-11-21 13:47:57 +0100

    Kivétel eldobása tasks completed & markdown updated

--- src/main/java/introexceptionthrow/patient/Patient.java
+++ src/main/java/introexceptionthrow/patient/Patient.java
@@ -6,6 +6,22 @@ public class Patient {
     private String ssn;
     private int yoB;
 
+    public static void main(String[] args) {
+        Patient patient = new Patient("Jack", "123456788", 1980);
+        System.out.println(patient.toString());
+        Patient patient1 = new Patient("John", "0", 1975);
+        System.out.println(patient1.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "name='" + name + '\'' +
+                ", ssn='" + ssn + '\'' +
+                ", yoB=" + yoB +
+                '}';
+    }
+
     public Patient(String name, String ssn, int yoB) {
         if (name.isBlank()) {
             throw new IllegalArgumentException("Name cannot be empty!");
@@ -15,6 +31,9 @@ public class Patient {
             if (!Character.isLetter(nameChar)) {
                 throw new IllegalArgumentException("Name is invalid!");
             }
+        }
+        if (!new SsnValidator().isValidSsn(ssn)) {
+            throw new IllegalArgumentException("Invalid SSN!");
         }
         if (yoB < MIN_YEAR) {
             throw new IllegalArgumentException("YoB is invalid!");
--- src/main/java/introexceptionthrow/patient/SsnValidator.java
+++ src/main/java/introexceptionthrow/patient/SsnValidator.java
@@ -5,5 +5,25 @@ public class SsnValidator {
         if (ssn.length() != 9) {
             return false;
         }
+        try {
+            int ssnSum = 0;
+            for (int i = 0; i < ssn.length(); i += 2) {
+                ssnSum += charConverterAndChecker(ssn.charAt(i)) * 3;
+            }
+            for (int i = 1; i < ssn.length(); i += 2) {
+                ssnSum += charConverterAndChecker(ssn.charAt(i)) * 7;
+            }
+            return (ssnSum % 10) == (charConverterAndChecker(ssn.charAt(8)));
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException.getMessage());
+            return false;
+        }
+    }
+
+    public int charConverterAndChecker(char ssnLetters) {
+        if (!Character.isDigit(ssnLetters)) {
+            throw new IllegalArgumentException("SSN should contain numbers only!");
+        }
+        return Integer.parseInt(String.valueOf(ssnLetters));
     }
 }
